805ab6404715e9940026da1356951ba0dfdda6a6
Shifa235 <108255166+Shifa235@users.noreply.github.com>
2022-07-15 17:02:22 +0530

    Add files via upload

---
+++ assignment5a/inc/header.h
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class words{
+	private:
+	        char *words[6];
+                int count;
+	public:
+	        words() {count = 0;}
+	        ~words()
