fbdd637eb99f27fb12edf78f2948532ab8b36951
Shifa235 <108255166+Shifa235@users.noreply.github.com>
2022-07-25 17:06:11 +0530

    Add files via upload

---
+++ multiAssignment1/multiAssignment1/inc/multiprocess.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <signal.h>
+#include <cstring>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fstream>
+#include <string>
+#include <time.h>
+
+#define MAXBUFF 1024
+
+using namespace std;
+
+void WriteInTheFile(char *);
+void ReadFromTheFile(char *);
+
